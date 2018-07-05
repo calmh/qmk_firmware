a27a0eb5f1c25711ec78cc2f364a35c6cc07214f
Jakob Borg <jakob@kastelo.net>
2018-07-05 19:21:52 +0200

    wip

--- keyboards/ergodox_ez/keymaps/calmh/keymap.c
+++ keyboards/ergodox_ez/keymaps/calmh/keymap.c
@@ -13,26 +13,26 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT_ergodox(
-      KC_EQUAL,   KC_1,     KC_2,     KC_3,     KC_4,   KC_5,   KC_HOME,
+      KC_ESCAPE,  KC_1,     KC_2,     KC_3,     KC_4,   KC_5,   KC_HOME,
       KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,   LGUI(KC_C),
       KC_GRAVE,   KC_A,     KC_S,     KC_D,     KC_F,   KC_G,
       KC_LSHIFT,  KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,   LGUI(KC_X),
       KC_LCTL,    KC_LALT,  KC_LGUI,  TT(1),    TT(2),
 
-                                KC_DELETE,  KC_ESCAPE,
+                                KC_DELETE,  KC_INSERT,
                                             KC_PGUP,
                     KC_SPACE,   KC_BSPACE,  KC_PGDOWN,
 
 
       KC_END,         KC_6,         KC_7,         KC_8,       KC_9,     KC_0,       KC_MINUS,
-      LGUI(KC_V),     KC_Y,         KC_U,         KC_I,       KC_O,     KC_P,       KC_SLASH,
+      LGUI(KC_V),     KC_Y,         KC_U,         KC_I,       KC_O,     KC_P,       KC_EQUAL,
                       KC_H,         KC_J,         KC_K,       KC_L,     KC_SCOLON,  KC_QUOTE,
-      LGUI(KC_SPACE), KC_N,         KC_M,         KC_COMMA,   KC_DOT,   KC_BSLASH,  KC_RSHIFT,
+      LGUI(KC_SPACE), KC_N,         KC_M,         KC_COMMA,   KC_DOT,   KC_BSLASH,  KC_SLASH,
                                   KC_LBRACKET,  KC_RBRACKET,  KC_LGUI,    KC_RALT,  KC_RCTRL,
 
                       KC_LEFT,  KC_RIGHT,
                       KC_UP,
-                      KC_DOWN,  RCTL(KC_C), KC_ENTER
+                      KC_DOWN,  KC_RSHIFT, KC_ENTER
     ),
 
   [1] = LAYOUT_ergodox(
