d39efd76de8f752c103f2f13841c9f40a855473b
Jakob Borg <jakob@kastelo.net>
2018-07-20 22:39:04 +0200

    wip

--- keyboards/ergodox_ez/keymaps/calmh/keymap.c
+++ keyboards/ergodox_ez/keymaps/calmh/keymap.c
@@ -10,29 +10,34 @@ enum custom_keycodes {
   RGB_SLD,
 };
 
+/*
+LALT(KC_SCOLON), // å
+LALT(KC_LBRACKET), // ä
+LALT(KC_RBRACKET), // ö
+*/
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT_ergodox(
-      KC_ESCAPE,  KC_1,     KC_2,     KC_3,     KC_4,   KC_5,   KC_HOME,
+      KC_ESCAPE,  KC_1,     KC_2,     KC_3,     KC_4,   KC_5,   TT(1),
       KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,   LGUI(KC_SPACE),
-      KC_GRAVE,   KC_A,     KC_S,     KC_D,     KC_F,   KC_G,
-      KC_LSHIFT,  KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,   KC_LOCK,
-      KC_LCTL,    KC_LALT,  KC_LGUI,  TT(1),    TT(2),
+      KC_LGUI,    KC_A,     KC_S,     KC_D,     KC_F,   KC_G,
+      KC_LSHIFT,  KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,   KC_MEDIA_NEXT_TRACK,
+      KC_LCTL,    KC_LALT,  KC_GRAVE,  KC_LBRACKET, KC_RBRACKET,
 
-                                KC_DELETE,  LALT(KC_SCOLON), // å
-                                            LALT(KC_LBRACKET), // ä
-                    KC_SPACE,   KC_BSPACE,  LALT(KC_RBRACKET), // ö
+                              KC_DELETE,   KC_POWER,
+                                           KC_PGUP,
+                    KC_SPACE, KC_BSPACE,   KC_PGDN,
 
 
-      KC_END,         KC_6,         KC_7,         KC_8,       KC_9,     KC_0,       KC_MINUS,
-      KC_LBRACKET,    KC_Y,         KC_U,         KC_I,       KC_O,     KC_P,       KC_EQUAL,
-                      KC_H,         KC_J,         KC_K,       KC_L,     KC_SCOLON,  KC_QUOTE,
-      KC_RBRACKET,    KC_N,         KC_M,         KC_COMMA,   KC_DOT,   KC_UP,      KC_RSHIFT,
-                                    KC_SLASH,     KC_BSLASH,  KC_LEFT,  KC_DOWN,    KC_RIGHT,
+      TT(2),              KC_6,         KC_7,         KC_8,       KC_9,     KC_0,       KC_MINUS,
+      KC_AUDIO_VOL_UP,    KC_Y,         KC_U,         KC_I,       KC_O,     KC_P,       KC_EQUAL,
+                          KC_H,         KC_J,         KC_K,       KC_L,     KC_SCOLON,  KC_QUOTE,
+      KC_AUDIO_VOL_DOWN,  KC_N,         KC_M,         KC_COMMA,   KC_DOT,   KC_SLASH,   KC_RSHIFT,
+              LALT(KC_SCOLON),  LALT(KC_LBRACKET), LALT(KC_RBRACKET),  KC_BSLASH,   KC_RCTL,
 
-                      LCMD(KC_LEFT),  LCMD(KC_RIGHT),
-                      KC_PGUP,
-                      KC_PGDOWN,  KC_RSHIFT, KC_ENTER
+                      KC_LEFT,  KC_RIGHT,
+                      KC_UP,
+                      KC_DOWN, KC_ENTER,  KC_SPACE
     ),
 
   [1] = LAYOUT_ergodox(
@@ -76,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
                     KC_MEDIA_PREV_TRACK,  KC_MEDIA_NEXT_TRACK,
                     KC_AUDIO_VOL_UP,
-                    KC_AUDIO_VOL_DOWN,    KC_AUDIO_MUTE, KC_MEDIA_PLAY_PAUSE
+                    KC_AUDIO_VOL_DOWN,    KC_MEDIA_PLAY_PAUSE,  KC_AUDIO_MUTE
 ),
 
 };
