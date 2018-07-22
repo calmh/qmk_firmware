99d33d4238400d9f7108421186bc4734523cf220
Jakob Borg <jakob@kastelo.net>
2018-07-22 17:34:10 +0200

    wip

--- keyboards/ergodox_ez/keymaps/calmh/keymap.c
+++ keyboards/ergodox_ez/keymaps/calmh/keymap.c
@@ -18,21 +18,21 @@ LALT(KC_RBRACKET), // ö
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT_ergodox(
-      KC_ESCAPE,  KC_1,     KC_2,     KC_3,     KC_4,   KC_5,   LGUI(KC_SPACE)                                        ,
-      KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,   KC_LCBR,
-      KC_LGUI,    KC_A,     KC_S,     KC_D,     KC_F,   KC_G,
+      KC_TRNS,    KC_1,     KC_2,     KC_3,     KC_4,   KC_5,   LGUI(KC_SPACE)                                        ,
+      KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,   KC_BSPACE,
+      KC_ESCAPE,  KC_A,     KC_S,     KC_D,     KC_F,   KC_G,
       KC_LSHIFT,  KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,   TT(1),
       KC_LCTL,    KC_LALT,  KC_GRAVE,  KC_LBRACKET, KC_RBRACKET,
 
                               KC_DELETE,   KC_POWER,
                                            KC_PGUP,
-                    KC_SPACE, KC_BSPACE,   KC_PGDN,
+                    KC_SPACE,  KC_LGUI,   KC_PGDN,
 
 
-      KC_TRNS, KC_6,         KC_7,         KC_8,       KC_9,     KC_0,       KC_MINUS,
-      KC_RCBR, KC_Y,         KC_U,         KC_I,       KC_O,     KC_P,       KC_EQUAL,
-               KC_H,         KC_J,         KC_K,       KC_L,     KC_SCOLON,  KC_QUOTE,
-      TT(2),   KC_N,         KC_M,         KC_COMMA,   KC_DOT,   KC_SLASH,   KC_RSHIFT,
+      KC_TRNS,   KC_6,         KC_7,         KC_8,       KC_9,     KC_0,       KC_MINUS,
+      KC_BSPACE, KC_Y,         KC_U,         KC_I,       KC_O,     KC_P,       KC_EQUAL,
+                 KC_H,         KC_J,         KC_K,       KC_L,     KC_SCOLON,  KC_QUOTE,
+      TT(2),     KC_N,         KC_M,         KC_COMMA,   KC_DOT,   KC_SLASH,   KC_RSHIFT,
               LALT(KC_SCOLON),  LALT(KC_LBRACKET), LALT(KC_RBRACKET),  KC_BSLASH,   KC_RCTL,
 
                       KC_LEFT,  KC_RIGHT,
