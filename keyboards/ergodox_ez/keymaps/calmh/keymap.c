5cdc76dcd7dc7d80983a72701c71bc78a8938a33
Jakob Borg <jakob@kastelo.net>
2019-02-10 21:33:00 +0100

    wip

--- keyboards/ergodox_ez/keymaps/calmh/keymap.c
+++ keyboards/ergodox_ez/keymaps/calmh/keymap.c
@@ -5,18 +5,14 @@
 
 enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
-  EPRM,
   VRSN,
-  RGB_SLD,
+  SV_AA,
+  SV_AE,
+  SV_OE,
 };
 
 /* docker run -e keymap=calmh -e keyboard=ergodox_ez --rm -v $('pwd'):/qmk:rw edasque/qmk_firmware */
 
-/*
-LALT(KC_SCOLON), // å
-LALT(KC_LBRACKET), // ä
-LALT(KC_RBRACKET), // ö
-*/
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT_ergodox(
@@ -35,7 +31,7 @@ LGUI(LCTL(KC_Q)), KC_6,         KC_7,      KC_8,      KC_9,     KC_0,       KC_M
   KC_RBRACKET,    KC_Y,         KC_U,      KC_I,      KC_O,     KC_P,       KC_EQUAL,
                   KC_H,         KC_J,      KC_K,      KC_L,     KC_SCOLON,  KC_QUOTE,
       KC_TRNS,    KC_N,         KC_M,      KC_COMMA,  KC_DOT,   KC_SLASH,   KC_RSHIFT,
-    MO(2),  KC_BSLASH, LALT(KC_SCOLON)/*å*/,  LALT(KC_LBRACKET)/*ä*/, LALT(KC_RBRACKET)/*ö*/,
+                               MO(2),     KC_BSLASH,  SV_AA,    SV_AE,      SV_OE,
 
                       KC_LEFT,  KC_RIGHT,
                       KC_UP,
@@ -79,7 +75,7 @@ LGUI(LCTL(KC_Q)), KC_6,         KC_7,      KC_8,      KC_9,     KC_0,       KC_M
        KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS,  KC_TRNS, KC_TRNS,
                 KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
-                         KC_TRNS, KC_TRNS, KC_TRNS,  KC_RIGHT, KC_TRNS,
+                         KC_TRNS, VRSN,    KC_TRNS,  KC_TRNS, KC_TRNS,
 
                     KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK,
                     KC_AUDIO_VOL_UP,
@@ -112,95 +108,87 @@ void matrix_init_user(void) {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  bool ls = keyboard_report->mods & MOD_BIT (KC_LSFT);
+  bool rs = keyboard_report->mods & MOD_BIT (KC_RSFT);
+
   switch (keycode) {
-    // dynamically generate these.
-    case EPRM:
+    case SV_AA:
       if (record->event.pressed) {
-        eeconfig_init();
+        SEND_STRING(SS_LALT("a"));
       }
       return false;
       break;
+
+    case SV_AE:
+      if (record->event.pressed) {
+        if (ls) {
+          unregister_code(KC_LSFT);
+        }
+        if (rs) {
+          unregister_code(KC_RSFT);
+        }
+
+        SEND_STRING(SS_LALT("u")); // dead key ¨
+
+        if (ls) {
+          register_code(KC_LSFT);
+        }
+        if (rs) {
+          register_code(KC_RSFT);
+        }
+
+        SEND_STRING("a");
+      }
+      return false;
+      break;
+
+    case SV_OE:
+      if (record->event.pressed) {
+        if (ls) {
+          unregister_code(KC_LSFT);
+        }
+        if (rs) {
+          unregister_code(KC_RSFT);
+        }
+
+        SEND_STRING(SS_LALT("u")); // dead key ¨
+
+        if (ls) {
+          register_code(KC_LSFT);
+        }
+        if (rs) {
+          register_code(KC_RSFT);
+        }
+
+        SEND_STRING("o");
+      }
+      return false;
+      break;
+
     case VRSN:
       if (record->event.pressed) {
         SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
       }
       return false;
       break;
-    case RGB_SLD:
-      if (record->event.pressed) {
-        rgblight_mode(1);
-      }
-      return false;
-      break;
-
   }
   return true;
 }
 
 uint32_t layer_state_set_user(uint32_t state) {
-
-    uint8_t layer = biton32(state);
-
-    ergodox_board_led_off();
     ergodox_right_led_1_off();
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
-    switch (layer) {
-      case 0:
-        #ifdef RGBLIGHT_COLOR_LAYER_0
-          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
-        #endif
-        break;
-      case 1:
-        ergodox_right_led_1_on();
-        #ifdef RGBLIGHT_COLOR_LAYER_1
-          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_1);
-        #endif
-        break;
-      case 2:
-        ergodox_right_led_2_on();
-        #ifdef RGBLIGHT_COLOR_LAYER_2
-          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_2);
-        #endif
-        break;
-      case 3:
-        ergodox_right_led_3_on();
-        #ifdef RGBLIGHT_COLOR_LAYER_3
-          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_3);
-        #endif
-        break;
-      case 4:
-        ergodox_right_led_1_on();
-        ergodox_right_led_2_on();
-        #ifdef RGBLIGHT_COLOR_LAYER_4
-          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_4);
-        #endif
-        break;
-      case 5:
-        ergodox_right_led_1_on();
-        ergodox_right_led_3_on();
-        #ifdef RGBLIGHT_COLOR_LAYER_5
-          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_5);
-        #endif
-        break;
-      case 6:
-        ergodox_right_led_2_on();
-        ergodox_right_led_3_on();
-        #ifdef RGBLIGHT_COLOR_LAYER_6
-          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
-        #endif
-        break;
-      case 7:
-        ergodox_right_led_1_on();
-        ergodox_right_led_2_on();
-        ergodox_right_led_3_on();
-        #ifdef RGBLIGHT_COLOR_LAYER_7
-          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
-        #endif
-        break;
-      default:
-        break;
-    }
-    return state;
 
+    if ((state & 0x2) != 0) {
+      ergodox_right_led_1_on();
+    }
+    if ((state & 0x4) != 0) {
+      ergodox_right_led_2_on();
+    }
+    if ((state & 0x8) != 0) {
+      ergodox_right_led_3_on();
+    }
+
+    return state;
 };
